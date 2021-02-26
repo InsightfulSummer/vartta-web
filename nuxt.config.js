9ef58967a958865ff47aab09d462cf2fd0bac2d6
Amir Haghighati <haghighati.amir@gmail.com>
2021-02-25 23:28:39 -0500

    fix: add multitabstate

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~/plugins/multiTabState.client.js' }],
   /*
    ** Nuxt.js dev-modules
    */
